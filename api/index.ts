8f6d58ae2743bc45fb4e30a03c0520dd091749ee
ClarissaCardarelli <ccardare@gmail.com>
2026-01-13 11:07:51 +0100

    Switch api entry to JS to avoid TS build on Vercel

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-// api/index.ts
-import serverless from "serverless-http";
-// @ts-ignore
-import app from "../server/dist/app.js";  // adjust path if your dist is inside server/
-
-console.log("api/index.ts loaded!");  // debug log
-
-export default serverless(app);
